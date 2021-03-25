ec127ed3d42c03e22f3d9d00998f4ae036489832
ka-create <81388009+ka-create@users.noreply.github.com>
2021-03-25 22:18:14 +0100

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var scrapeButton = document.getElementById('clickToScrape');
+    scrapeButton.addEventListener('click', function() {
+  
+      
+      })
