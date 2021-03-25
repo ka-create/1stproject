779aac86fc44fb253efa2bc887aadbb01b87be48
hanios123 <hanios123@gmail.com>
2021-03-25 22:53:35 +0100

    scrapping , added api call with axios

--- popup.js
+++ popup.js
@@ -1,6 +1,17 @@
 document.addEventListener('DOMContentLoaded', function() {
     var scrapeButton = document.getElementById('clickToScrape');
     scrapeButton.addEventListener('click', function() {
-  
-      
-      })
+           callScappingAPi()
+    })
+
+    function callScappingAPi(){
+       axios.post('https://api.example.com',{url:'myurl'}).then((response)=>{
+          //here where you receive your information you should write some logice to handle the data
+          console.log(response.data)
+       }).catch((error) =>{
+         console.error(error);
+       }).finally(()=>{
+          //you can do anything you want no matter the result is
+       })
+
+    }
